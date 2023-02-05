cbf86fb4f5c558be1406bc04fa2d20316a8f85fe
Fatih Furkan Kocabiyik <82720725+ffurkan10@users.noreply.github.com>
2023-02-05 20:21:10 +0300

    add description

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -5,23 +5,26 @@ import Input from "../../components/input/Input";
 
 const Home = ({ coin, setCoin }) => {
   const favList = [
+    //Favorilere eklenecek olan coin'in bilgilerinin tutulduğu bir dizi.
     {
       id: 7,
       details: "Price",
     },
   ];
 
-  const [filteredList, setFilteredList] = useState([]);
-  const [search, setSearch] = useState("");
-  const [favorites, setFavorites] = useState([]);
+  const [filteredList, setFilteredList] = useState([]); //Kullanıcının arama yaptığı zaman filtrelenen coin verilerinin tutulduğu state.
+  const [search, setSearch] = useState(""); // Kullanıcının input'a yazdığı değerin tutulduğu state.
+  const [favorites, setFavorites] = useState([]); //Kullanıcının favori ettiği coin'lerin verilerinin tutulduğu state.
 
   useEffect(() => {
-    setFilteredList(coin);
+    setFilteredList(coin); //filteredList state'ine başlangıçta tüm coin verileri atanır.
   }, [coin]);
 
   const filterCoins = (e) => {
+    //Kullanıcının input'a yazdığı değer kullanılarak coin verileri filtrelenir ve filtrelenen veriler filteredList state'ine atanır.
     const coinData = coin?.filter(
-      (obj) => obj.name?.toLowerCase().indexOf(e.toLowerCase()) > -1
+      // değerin tüm karakterlerinin küçük harf halini verir.
+      (obj) => obj.name?.toLowerCase().indexOf(e.toLowerCase()) > -1 // aranılan değerin bulunup bulunmadığını kontrol eder, bulunmazsa -1 verir
     );
     setFilteredList(coinData);
     setSearch(e);
@@ -29,11 +32,12 @@ const Home = ({ coin, setCoin }) => {
 
   const handleFavorites = (id) => {
     const newFavorite = coin.find((coin) => coin.id === id);
-    const already = favorites.find((favorite) => favorite.id === id);
+    const already = favorites.find((favorite) => favorite.id === id); // verilen "id" ile eşleşen bir para birimi aranır.
     if (already) {
-      setFavorites(favorites.filter((favorites) => favorites.id !== id));
+      // "true" değerini içeriyorsa
+      setFavorites(favorites.filter((favorites) => favorites.id !== id)); // "favorites.filter" methodu ile filtrelenir ve silinir.
     } else {
-      setFavorites([...favorites, newFavorite]);
+      setFavorites([...favorites, newFavorite]); // eklenmemişse "favorites" listesine eklenir.
     }
   };
   return (
